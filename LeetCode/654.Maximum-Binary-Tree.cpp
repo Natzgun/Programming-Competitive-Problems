aaa7aacb66f195330e2684c6640633442e1433d0
Natzgun <itzlilouxx@gmail.com>
2024-01-30 11:54:09 -0500

    EL primer if es para el caso en el que el valor del nuevo nodo sea mayor a los que tiene encima, en ese caso se convierte en la nueva raiz por lo que se llena el array de parent con su posicion, y el left de ese nodo sera el arbol anterior

--- LeetCode/654.Maximum-Binary-Tree.cpp
+++ LeetCode/654.Maximum-Binary-Tree.cpp
@@ -21,7 +21,19 @@ struct TreeNode* constructMaximumBinaryTree(int* nums, int numsSize) {
   int root{0}, last;
 
   for (int i = 1; i <= numsSize - 1; i++) {
-    
+    // Guardamos la posicion del elemento trabajado anteriormente
+    last -= 1;
+    // Este es para el caso en el que se encuentra un nodo con valor mayor al nuevo nodo
+    // Guarda la ultima derecha que tenia que en este caso es vacia
+    rightchild[i] = -1;
+
+    if (nums[last] < nums[i]) {
+      parent[root] = i;
+      leftchild[i] = last;
+      root++;
+    } else if (nums[last] > nums[i]) {
+      rightchild[last] = i; 
+    }
   }
 }
 
