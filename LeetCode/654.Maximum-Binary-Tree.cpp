f53f56a7d88b29ac6c8d5fe69dcc84f92f1bd0bb
Natzgun <itzlilouxx@gmail.com>
2024-01-30 11:12:30 -0500

    Se define la estructura para avanzar crear un arbol cartesiano dada una secuencia de n numeros

---
+++ LeetCode/654.Maximum-Binary-Tree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+ // Definition for a binary tree node.
+struct TreeNode {
+  int data;
+  struct TreeNode *left;
+  struct TreeNode *right;
+};
+
+
+struct TreeNode* constructMaximumBinaryTree(int* nums, int numsSize) {
+  TreeNode* tmp_parent = new TreeNode;
+}
+
+int main () {
+  int inorder[] = {3,2,1,6,0,5};
+  int len = sizeof(inorder)/sizeof(inorder[0]);
+  cout << len << endl;
+  return 0;
+}
