e1efc941dea7d027bddbd2f44065110a3793a857
Natzgun <itzlilouxx@gmail.com>
2024-02-23 21:22:05 -0500

    Se uso recursion para realizar la suma de rangos, pero se podria haber utilizado otras estructuras de datos, por lo que el codigo no es gurú de momento

---
+++ LeetCode/938.Range-Sum-of-BST.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+  int val;
+  struct TreeNode *left;
+  struct TreeNode *right;
+};
+
+void transversal(struct TreeNode *root, int *sum, int low, int high) {
+  if (!root)
+    return;
+
+  transversal(root->left, sum, low, high);
+  if (root->val >= low && root->val <= high)
+    *sum = root->val;
+  transversal(root->right, sum, low, high);
+}
+
+int rangeSumBST(struct TreeNode *root, int low, int high) {
+  int sum = 0;
+  transversal(root, &sum, low, high);
+  return sum;
+}
+
+void sumTest(int *sum) { *sum = 20; }
+
+void test() {
+  int sum = 0;
+  sumTest(&sum);
+  printf("%d", sum);
+}
+
+int main() {
+  test();
+  return 0;
+}
