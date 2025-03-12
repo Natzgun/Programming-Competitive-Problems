64c793037c30e82da158a7f4f73ee22f4f325c00
Natzgun <itzlilouxx@gmail.com>
2025-03-11 21:11:27 -0500

    I need more maths bro...

---
+++ CodeForces/Dima_and_Friends.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, start, end) for (int i = (start); i < (end); i++)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define ini(t) \
+  int(t);      \
+  cin >> t;
+#define inll(t) \
+  ll(t);        \
+  cin >> t;
+#define ins(t) \
+  string(t);   \
+  cin >> t;
+#define inp(t) \
+  int(t);      \
+  cin >> t;
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+  ini(n);
+  int sum{0};
+
+  for (size_t i = 0; i < n; i++) {
+    ini(f);
+    sum += f;
+  }
+
+  int cont{0};
+  for (int i = 1; i <= 5; i++) {
+    if ((sum + i) % (n + 1) != 1) cont++;
+  }
+
+  cout << cont << "\n";
+
+  return 0;
+}
