5411dc3841c67c6b11e0be8b4a3df852dbc1d7da
Natzgun <itzlilouxx@gmail.com>
2025-03-24 09:51:46 -0500

    Chad?

---
+++ CodeForces/Hexadecimals_theorem.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, start, end) for (int i = (start); i < (end); i++)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define ini(t) int(t); cin >> t;
+#define inll(t) ll(t); cin >> t;
+#define ins(t) string(t); cin >> t;
+#define inp(t) int (t); cin >> t;
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+  inll(n);
+
+  cout << 0 << " " << 0 << " " << n;
+
+  return 0;
+}
