2064389984d0d6354187b1b01c72db77adeb5f5c
Natzgun <itzlilouxx@gmail.com>
2025-04-03 15:57:12 -0500

    Need more math...

---
+++ CodeForces/The_number_of_positions.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, start, end) for (int i = (start); i < (end); i++)
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define ini(t)                                                                 \
+  int(t);                                                                      \
+  cin >> t;
+#define inll(t)                                                                \
+  ll(t);                                                                       \
+  cin >> t;
+#define ins(t)                                                                 \
+  string(t);                                                                   \
+  cin >> t;
+#define inp(t)                                                                 \
+  int(t);                                                                      \
+  cin >> t;
+
+int main() {
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+  ini(n);
+  ini(a);
+  ini(b);
+
+  int curr_pos = n - b;
+  while (true) {
+    if (a >= curr_pos - 1)
+      break;
+    else
+      curr_pos++;
+  }
+
+  cout << (n - curr_pos) + 1 << "\n";
+
+  return 0;
+}
